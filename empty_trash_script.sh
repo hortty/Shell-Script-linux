8bce0f2cb9d8d2fd84463c5a9ae561e4bd64f9f5
Gabriel Bueno <gabrielhortmann@gmail.com>
2022-10-12 19:00:30 +0000

    Empty trash script

---
+++ empty_trash_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "O que deseja fazer, limpar todos os arquivos do lixo ou restaurar os arquivos na pasta trash_files dentro do diretorio $PWD"
+echo
+echo "1 para apenas limpar"
+echo "2 para trazer arquivos para pasta trash_files"
+read options
+if [ $options -eq 1 ]
+then
+	echo "os seguintes arquivos estao sendo excluidos:"
+	echo 
+	ls -la /home/$USER/.local/share/Trash/
+	echo
+	echo "limpando o lixo..."
+	rm -rf /home/$USER/.local/share/Trash/*
+else
+	if [ -d $PWD/trash_files ]
+	then
+		echo "trash_files ja existe, removendo esse diretorio..."
+		rm -rf trash_files/
+		echo "agora sera criado uma nova pasta"
+	fi
+	echo "criando diretorio trash_files aqui nessa pasta $PWD"
+	mkdir trash_files
+	echo "movendo arquivos"
+	mv /home/$USER/.local/share/Trash/* trash_files/
+fi
+echo "prontinho!"
